7be40c732c16e90a0d267a2f57c13dc2072df356
Michał Poręba <michalporeba@gmail.com>
2020-08-28 23:05:20 +0100

    setting up the test data - full script

--- pstool/Reset-TestData.ps1
+++ pstool/Reset-TestData.ps1
@@ -1,3 +1,33 @@
 $root = "./TestData"
 
-New-Item -Path "$root/Dev" -ItemType directory 
+if (Test-Path -Path $root) {
+    Remove-Item -Path $root -Recurse
+}
+
+New-Item -Path "$root/Dev" -ItemType directory -Force
+New-Item -Path "$root/Dev/Server10.txt" -ItemType file
+Set-Content -Path "$root/Dev/Server10.txt" -Value "OK"
+New-Item -Path "$root/Dev/Server11.txt" -ItemType file
+Set-Content -Path "$root/Dev/Server11.txt" -Value "OK"
+New-Item -Path "$root/Dev/Server12.txt" -ItemType file
+Set-Content -Path "$root/Dev/Server12.txt" -Value "OK"
+
+New-Item -Path "$root/Test" -ItemType directory -Force
+New-Item -Path "$root/Test/Server20.txt" -ItemType file
+New-Item -Path "$root/Test/Server22.txt" -ItemType file
+Add-Content -Path "$root/Test/Server22.txt" -Value "line1"
+Add-Content -Path "$root/Test/Server22.txt" -Value "line2"
+Add-Content -Path "$root/Test/Server22.txt" -Value "line3"
+Add-Content -Path "$root/Test/Server22.txt" -Value "line4"
+
+New-Item -Path "$root/Prod" -ItemType directory -Force
+New-Item -Path "$root/Prod/Server31.txt" -ItemType file
+Set-Content -Path "$root/Prod/Server31.txt" -Value "OK"
+New-Item -Path "$root/Prod/Server32.txt" -ItemType file
+New-Item -Path "$root/Prod/Server33.txt" -ItemType file
+Set-Content -Path "$root/Prod/Server33.txt" -Value "OK"
+New-Item -Path "$root/Prod/Server35.txt" -ItemType file
+Add-Content -Path "$root/Prod/Server35.txt" -Value "line1"
+Add-Content -Path "$root/Prod/Server35.txt" -Value "line2"
+Add-Content -Path "$root/Prod/Server35.txt" -Value "line3"
+Add-Content -Path "$root/Prod/Server35.txt" -Value "line4"
