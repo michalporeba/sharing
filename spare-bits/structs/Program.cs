0705405683059f455756ad231b34d3fb32270864
Michał Poręba <michalporeba@gmail.com>
2020-11-23 19:50:01 +0000

    c# struct experiments

---
+++ spare-bits/structs/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace structs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rand = new Random();
+            
+            for (var i = 0; i < 100000; ++i)
+            {
+                var s = new SimpleStruct()
+                {
+                    A = rand.Next(10),
+                    B = rand.Next(10),
+                    C = rand.Next(10),
+                    D = rand.Next(10)
+                };    
+                Print(s);
+            }
+            
+            
+        }
+
+        private static void Print(SimpleStruct s)
+        {
+            Console.Out.WriteLine(s.A+s.B+s.C+s.D);
+        }
+    }
+}
---
+++ spare-bits/structs/SimpleStruct.cs
@@ -0,0 +1,10 @@
+namespace structs
+{
+    public struct SimpleStruct
+    {
+        public int A;
+        public int B;
+        public int C;
+        public int D;
+    }
+}
